47ef2f0850b1b1fa04c9ba96c291dcd323aefb59
lezarn <landy6080860908@yahoo.com.tw>
2019-12-23 16:42:03 +0800

    add main()

--- src/com/company/letterGrade.java
+++ src/com/company/letterGrade.java
@@ -1,20 +1,23 @@
 package com.company;
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class letterGrade {
 
-    public static char letterGrade(int score){
+    public static char letterGrade(int score) {
         char grade;
-        if(score < 0 || score > 100)
+        if (score < 0 || score > 100)
             grade = 'X';
-        else if (score>=90 && score <= 100)
+        else if (score >= 90 && score <= 100)
             grade = 'A';
-        else if (score>=80 && score < 90)
+        else if (score >= 80 && score < 90)
             grade = 'B';
-        else if (score>=70 && score < 80)
+        else if (score >= 70 && score < 80)
             grade = 'C';
-        else if (score>=60 && score < 70)
+        else if (score >= 60 && score < 70)
             grade = 'D';
         else
             grade = 'F';
@@ -22,6 +25,22 @@ public class letterGrade {
 
     }
 
+    public static void main(String[] args) {
 
+        System.out.print("Enter the score = ");
+        try {
+            InputStreamReader isr = new InputStreamReader(System.in);
+            BufferedReader br = new BufferedReader(isr);
+            int score = Integer.parseInt(br.readLine());
+            char grade = letterGrade(score);
+            System.out.println("The grade of " + score + " is " + grade);
+        } catch (NumberFormatException ex) {
+            System.out.println("Not an integer!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 
 }
